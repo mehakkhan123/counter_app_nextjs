a752cb52aa7394986b0464ef8b5b9618e5ecbfff
mehakkhan123 <159705245+mehakkhan123@users.noreply.github.com>
2025-07-17 11:24:01 +0500

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -15,7 +15,7 @@ export default function Counter(){
             <div className="flex flex-col gap-10 items-center justify-center p-9 shadow-md rounded-xl">
             <h1 className="text-3xl text-center">Counter App</h1>
             <div className="flex flex-row gap-4 justify-center items-center">
-            <button className="bg-blue-950 text-white text-4xl px-7 rounded-md cursor-pointer hover:bg-blue-900" onClick={Add}>+</button>
+            <button className="bg-blue-900 text-white text-4xl px-7 rounded-md cursor-pointer hover:bg-blue-900" onClick={Add}>+</button>
             <h1 className="font-bold text-2xl">{count}</h1>
             <button className="bg-amber-950 text-white text-4xl px-7 rounded-md cursor-pointer hover:bg-amber-900" onClick={Subtract}>-</button>
             </div>
